3aa658999bfd85929da8c7c1752c63ccc1d972c2
Bernt Gudmestad <bernt@xspeed.co>
2017-01-06 18:25:40 -0800

    changed so save test download functions are only set by mosaico if they havent been defined as plugins passed to mosaico init

--- src/js/ext/localstorage.js
+++ src/js/ext/localstorage.js
@@ -28,75 +28,82 @@ var lsLoader = function(hash_key, emailProcessorBackend, templateLoader, callbac
 
 var lsCommandPluginFactory = function(md, emailProcessorBackend) {
   var commandsPlugin = function(mdkey, mdname, viewModel) {
+    //only configure save function if not already configured, like by plugins passed to Mosaico.init()
+    if (typeof viewModel.save == 'undefined'){
+      var saveCmd = {
+        name: 'Save', // l10n happens in the template
+        enabled: ko.observable(true)
+      };
+      saveCmd.execute = function() {
+        saveCmd.enabled(false);
+        viewModel.metadata.changed = Date.now();
+        if (typeof viewModel.metadata.key == 'undefined') {
+          console.warn("Unable to find ket in metadata object...", viewModel.metadata);
+          viewModel.metadata.key = mdkey;
+        }
+        global.localStorage.setItem("metadata-" + mdkey, viewModel.exportMetadata());
+        global.localStorage.setItem("template-" + mdkey, viewModel.exportJSON());
+        saveCmd.enabled(true);
+      };
+      viewModel.save = saveCmd;
+    }
 
-    // console.log("loading from metadata", md, model);
-    var saveCmd = {
-      name: 'Save', // l10n happens in the template
-      enabled: ko.observable(true)
-    };
-    saveCmd.execute = function() {
-      saveCmd.enabled(false);
-      viewModel.metadata.changed = Date.now();
-      if (typeof viewModel.metadata.key == 'undefined') {
-        console.warn("Unable to find ket in metadata object...", viewModel.metadata);
-        viewModel.metadata.key = mdkey;
-      }
-      global.localStorage.setItem("metadata-" + mdkey, viewModel.exportMetadata());
-      global.localStorage.setItem("template-" + mdkey, viewModel.exportJSON());
-      saveCmd.enabled(true);
-    };
-    var testCmd = {
-      name: 'Test', // l10n happens in the template
-      enabled: ko.observable(true)
-    };
-    var downloadCmd = {
-      name: 'Download', // l10n happens in the template
-      enabled: ko.observable(true)
-    };
-    testCmd.execute = function() {
-      testCmd.enabled(false);
-      var email = global.localStorage.getItem("testemail");
-      if (email === null || email == 'null') email = viewModel.t('Insert here the recipient email address');
-      email = global.prompt(viewModel.t("Test email address"), email);
-      if (email.match(/@/)) {
-        global.localStorage.setItem("testemail", email);
-        console.log("TODO testing...", email);
-        var postUrl = emailProcessorBackend ? emailProcessorBackend : '/dl/';
-        var post = $.post(postUrl, {
-          action: 'email',
-          rcpt: email,
-          subject: "[test] " + mdkey + " - " + mdname,
-          html: viewModel.exportHTML()
-        }, null, 'html');
-        post.fail(function() {
-          console.log("fail", arguments);
-          viewModel.notifier.error(viewModel.t('Unexpected error talking to server: contact us!'));
-        });
-        post.success(function() {
-          console.log("success", arguments);
-          viewModel.notifier.success(viewModel.t("Test email sent..."));
-        });
-        post.always(function() {
+    if (typeof viewModel.test == 'undefined'){
+      var testCmd = {
+        name: 'Test', // l10n happens in the template
+        enabled: ko.observable(true)
+      };
+      testCmd.execute = function() {
+        testCmd.enabled(false);
+        var email = global.localStorage.getItem("testemail");
+        if (email === null || email == 'null') email = viewModel.t('Insert here the recipient email address');
+        email = global.prompt(viewModel.t("Test email address"), email);
+        if (email.match(/@/)) {
+          global.localStorage.setItem("testemail", email);
+          console.log("TODO testing...", email);
+          var postUrl = emailProcessorBackend ? emailProcessorBackend : '/dl/';
+          var post = $.post(postUrl, {
+            action: 'email',
+            rcpt: email,
+            subject: "[test] " + mdkey + " - " + mdname,
+            html: viewModel.exportHTML()
+          }, null, 'html');
+          post.fail(function() {
+            console.log("fail", arguments);
+            viewModel.notifier.error(viewModel.t('Unexpected error talking to server: contact us!'));
+          });
+          post.success(function() {
+            console.log("success", arguments);
+            viewModel.notifier.success(viewModel.t("Test email sent..."));
+          });
+          post.always(function() {
+            testCmd.enabled(true);
+          });
+        } else {
+          global.alert(viewModel.t('Invalid email address'));
           testCmd.enabled(true);
-        });
-      } else {
-        global.alert(viewModel.t('Invalid email address'));
-        testCmd.enabled(true);
-      }
-    };
-    downloadCmd.execute = function() {
-      downloadCmd.enabled(false);
-      viewModel.notifier.info(viewModel.t("Downloading..."));
-      viewModel.exportHTMLtoTextarea('#downloadHtmlTextarea');
-      var postUrl = emailProcessorBackend ? emailProcessorBackend : '/dl/';
-      global.document.getElementById('downloadForm').setAttribute("action", postUrl);
-      global.document.getElementById('downloadForm').submit();
-      downloadCmd.enabled(true);
-    };
+        }
+      };
+      viewModel.test = testCmd;
+    }
 
-    viewModel.save = saveCmd;
-    viewModel.test = testCmd;
-    viewModel.download = downloadCmd;
+    if (typeof viewModel.download == 'undefined'){
+      var downloadCmd = {
+        name: 'Download', // l10n happens in the template
+        enabled: ko.observable(true)
+      };
+      downloadCmd.execute = function() {
+        downloadCmd.enabled(false);
+        viewModel.notifier.info(viewModel.t("Downloading..."));
+        viewModel.exportHTMLtoTextarea('#downloadHtmlTextarea');
+        var postUrl = emailProcessorBackend ? emailProcessorBackend : '/dl/';
+        global.document.getElementById('downloadForm').setAttribute("action", postUrl);
+        global.document.getElementById('downloadForm').submit();
+        downloadCmd.enabled(true);
+      };
+      viewModel.download = downloadCmd;
+    }
+    
   }.bind(undefined, md.key, md.name);
 
   return commandsPlugin;
